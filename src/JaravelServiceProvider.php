019e8ed6a501ebaf8e7d3b5aef2798553dcdc370
sergey.gna <sergey.gna@okwork.io>
2021-08-24 16:03:40 +0300

    fix

--- src/JaravelServiceProvider.php
+++ src/JaravelServiceProvider.php
@@ -85,7 +85,6 @@ class JaravelServiceProvider extends ServiceProvider
 
         $config = new Config(
             [
-                'trace_id_header' => ConfigRepository::get('jaravel.trace_id_header', 'X-Trace-Id'),
                 'sampler' => [
                     'type' => Jaeger\SAMPLER_TYPE_CONST,
                     'param' => true,
@@ -95,7 +94,8 @@ class JaravelServiceProvider extends ServiceProvider
                     "reporting_host" => ConfigRepository::get('jaravel.agent_host', '127.0.0.1'),
                     "reporting_port" => ConfigRepository::get('jaravel.agent_port', 6831),
                 ],
-                'dispatch_mode' => Config::JAEGER_OVER_BINARY_UDP,
+                'trace_id_header' => ConfigRepository::get('jaravel.trace_id_header', 'X-Trace-Id'),
+                //'dispatch_mode' => Config::JAEGER_OVER_BINARY_UDP,
             ],
             ConfigRepository::get('jaravel.tracer_name', 'application'),
         );
